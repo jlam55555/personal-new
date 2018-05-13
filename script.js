5fa79ad5d4f5010e62e2ba76997c3f72102afdf8
Jonathan Lam <jlam55555@gmail.com>
2018-05-13 10:21:24 -0400

    prevent constnat resizing from vertical resize

--- script.js
+++ script.js
@@ -9,21 +9,26 @@ let imageSeparators = document.querySelectorAll('.image-separator');
 let firstJumbotron = document.querySelector('#main-jumbotron');
 let videoSeparator = document.querySelector('.video-separator');
 let videoPlaceholder = document.querySelector('.video-placeholder');
+let oldWidth = 0;
 let resizeHandler = () => {
   firstJumbotron.style.height = window.innerHeight + 'px';
-  [].forEach.call(imageSeparators, imageSeparator => {
-    // mobile
-    if(window.innerWidth < 750 || window.innerHeight < 700)
-      imageSeparator.style.backgroundSize = `auto ${window.innerHeight}px`;
-    // desktop
-    else
-      imageSeparator.style.backgroundSize = `${window.innerWidth}px auto`;
-    imageSeparator.style.height = window.innerHeight + 'px';
-  });
-  videoPlaceholder.style.height = window.innerHeight + 'px';
-  videoSeparator.style.minHeight = window.innerHeight + 'px';
-  videoSeparator.style.minWidth = window.innerWidth + 'px';
-  videoSeparator.style.marginLeft = (window.innerWidth - videoSeparator.offsetWidth) / 2 + 'px';
+  // prevent constant refreshing on mobile on vertical resize
+  if(oldWidth !== window.innerWidth) {
+    oldWidth = window.innerWidth;
+    [].forEach.call(imageSeparators, imageSeparator => {
+      // mobile
+      if(window.innerWidth < 750 || window.innerHeight < 700)
+        imageSeparator.style.backgroundSize = `auto ${window.innerHeight}px`;
+      // desktop
+      else
+        imageSeparator.style.backgroundSize = `${window.innerWidth}px auto`;
+      imageSeparator.style.height = window.innerHeight + 'px';
+    });
+    videoPlaceholder.style.height = window.innerHeight + 'px';
+    videoSeparator.style.minHeight = window.innerHeight + 'px';
+    videoSeparator.style.minWidth = window.innerWidth + 'px';
+    videoSeparator.style.marginLeft = (window.innerWidth - videoSeparator.offsetWidth) / 2 + 'px';
+  }
 };
 resizeHandler();
 window.addEventListener('resize', resizeHandler);
