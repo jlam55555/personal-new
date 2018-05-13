87fe44f4f796d7e43899c37c0f7b425364833c08
Jonathan Lam <jlam55555@gmail.com>
2018-05-13 10:12:55 -0400

    changed background size

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let videoPlaceholder = document.querySelector('.video-placeholder');
 let resizeHandler = () => {
   firstJumbotron.style.height = window.innerHeight + 'px';
   [].forEach.call(imageSeparators, imageSeparator => {
-    imageSeparator.style.backgroundSize = `${window.innerWidth}px auto`;
+    imageSeparator.style.backgroundSize = `auto ${window.innerHeight}px`;
     imageSeparator.style.height = window.innerHeight + 'px';
   });
   videoPlaceholder.style.height = window.innerHeight + 'px';
