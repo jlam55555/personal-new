6c63a1ddb5b7c63466840f88350a0f79f3fecebc
Jonathan Lam <jlam55555@gmail.com>
2018-05-14 23:24:14 -0400

    trying to fix error

--- script.js
+++ script.js
@@ -60,6 +60,7 @@ let t = setInterval(() => {
 
 // dropdown button
 let dropdownButton = document.querySelector('#dropdown-button');
+let dropdown = document.querySelector('#dropdown');
 //let dropdown = document.querySelector('#dropdown');
 /*
 dropdown.classList.add('show');
